9eaa08c4bdd465f34e9133d6e3f366cf1ebd72c3
Crowdbotics <team@crowdbotics.com>
2023-09-11 18:48:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-band-43337",
+  url: "https://spring-band-43337.botics.co",
+  api: "https://spring-band-43337.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
